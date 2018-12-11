afaaa634e02808ad9713cb33d473e823fb7cecdb
HUNGDT <hk2t.thaihung@gmail.com>
2018-12-11 11:28:35 +0700

    done

---
+++ src/test/java/Point2DTest.java
@@ -0,0 +1,16 @@
+import com.codegym.Point2D;
+import com.codegym.Point3D;
+
+import java.util.Arrays;
+
+public class Point2DTest {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D();
+
+        Point2D point2D1 = new Point2D(25.9f, 16.11f);
+
+        System.out.println(point2D);
+        System.out.println(point2D1);
+    }
+
+}
---
+++ src/test/java/Point3DTest.java
@@ -0,0 +1,13 @@
+import com.codegym.Point3D;
+
+public class Point3DTest {
+    public static void main(String[] args) {
+        Point3D point3D = new Point3D();
+        System.out.println(point3D);
+
+        Point3D point3D1 = new Point3D(9.25f, 11.16f, 20.19f);
+        System.out.println(point3D1);
+    }
+
+
+}
